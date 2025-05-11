1e91033c5fda0f18a0a8efa37b153820fe5433c5
ajroot5685 <anjeong8577@naver.com>
2025-05-11 14:07:33 +0900

    init: 공통 응답 수정 및 CustomException 추가

--- insty-common/src/main/java/insty/response/ErrorInfo.java
+++ insty-common/src/main/java/insty/response/ErrorInfo.java
@@ -6,7 +6,7 @@ public record ErrorInfo<T>(
         T details
 ) {
 
-    private static <T> ErrorInfo<T> createErrorInfo(ErrorCode errorCode, T details, String customMessage) {
+    private static <T> ErrorInfo<T> createErrorInfo(ErrorCode errorCode, String customMessage, T details) {
         return new ErrorInfo<>(
                 errorCode.getCode(),
                 customMessage != null ? customMessage : errorCode.getMessage(),
@@ -19,14 +19,14 @@ public record ErrorInfo<T>(
     }
 
     public static <T> ErrorInfo<T> of(ErrorCode errorCode, String customMessage) {
-        return ErrorInfo.createErrorInfo(errorCode, null, customMessage);
+        return ErrorInfo.createErrorInfo(errorCode, customMessage, null);
     }
 
     public static <T> ErrorInfo<T> ofWithDetails(ErrorCode errorCode, T details) {
-        return ErrorInfo.createErrorInfo(errorCode, details, null);
+        return ErrorInfo.createErrorInfo(errorCode, null, details);
     }
 
-    public static <T> ErrorInfo<T> ofWithDetails(ErrorCode errorCode, T details, String message) {
-        return ErrorInfo.createErrorInfo(errorCode, details, message);
+    public static <T> ErrorInfo<T> ofWithDetails(ErrorCode errorCode, T details, String customMessage) {
+        return ErrorInfo.createErrorInfo(errorCode, customMessage, details);
     }
 }
--- insty-common/src/main/java/insty/response/FailRes.java
+++ insty-common/src/main/java/insty/response/FailRes.java
@@ -4,4 +4,8 @@ public record FailRes<T>(
         boolean success,
         ErrorInfo<T> error
 ) {
+
+    public static <T> FailRes<T> of(ErrorInfo<T> error) {
+        return new FailRes<>(false, error);
+    }
 }
