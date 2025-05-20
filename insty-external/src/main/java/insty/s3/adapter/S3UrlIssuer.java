437ac380d64adeddb1a692418d4eca0b87f7f5c4
ajroot5685 <anjeong8577@naver.com>
2025-05-20 17:16:38 +0900

    ✨ Feat(s3): pre-signed URL 발급

---
+++ insty-external/src/main/java/insty/s3/adapter/S3UrlIssuer.java
@@ -0,0 +1,41 @@
+package insty.s3.adapter;
+
+import insty.s3.constant.S3Constants;
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+@Service
+public class S3UrlIssuer {
+
+    private final S3Presigner s3Presigner;
+    private final String bucket;
+
+    public S3UrlIssuer(
+            S3Presigner s3Presigner,
+            @Value("${aws.s3-upload-bucket}") String bucket
+    ) {
+        this.s3Presigner = s3Presigner;
+        this.bucket = bucket;
+    }
+
+    public String generatePresignedUrl(String key, String contentType) {
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .contentType(contentType)
+                .build();
+
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .putObjectRequest(objectRequest)
+                .signatureDuration(Duration.ofMinutes(S3Constants.URL_EXPIRATION_MINUTES))
+                .build();
+
+        PresignedPutObjectRequest presigned = s3Presigner.presignPutObject(presignRequest);
+        return presigned.url().toString();
+    }
+}
