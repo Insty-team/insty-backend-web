6386f8487e8a0334e92ffb3d8fa018be5445c2b1
ajroot5685 <anjeong8577@naver.com>
2025-05-22 19:31:55 +0900

    ✅ Test: 인수테스트 시 외부 의존성 Mocking

--- insty-external/src/main/java/insty/s3/config/AwsS3Config.java
+++ insty-external/src/main/java/insty/s3/config/AwsS3Config.java
@@ -3,12 +3,14 @@ package insty.s3.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
+@Profile("!test")
 public class AwsS3Config {
 
     @Value("${aws.credentials.access-key}")
