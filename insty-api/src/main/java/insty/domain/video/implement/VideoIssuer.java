113e033a2c3b6a13598b07d1d57c212d3cce39c1
ajroot5685 <anjeong8577@naver.com>
2025-05-23 20:36:09 +0900

    ✨ Feat(video): CloudFront Sign 로직

--- insty-api/src/main/java/insty/domain/video/implement/VideoIssuer.java
+++ insty-api/src/main/java/insty/domain/video/implement/VideoIssuer.java
@@ -1,7 +1,10 @@
 package insty.domain.video.implement;
 
+import insty.cloudfront.adapter.CloudFrontSigner;
 import insty.s3.adapter.S3UrlIssuer;
 import insty.s3.dto.PresignedUrlDto;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +13,22 @@ import org.springframework.stereotype.Service;
 public class VideoIssuer {
 
     private final S3UrlIssuer s3UrlIssuer;
+    private final CloudFrontSigner cloudFrontSigner;
 
     public PresignedUrlDto getUploadInfo(String s3Key, String contentType) {
         return s3UrlIssuer.generatePresignedUrl(s3Key, contentType);
+    }
+
+    public String signM3u8Content(String m3u8Content) {
+        return Arrays.stream(m3u8Content.split("\n"))
+                .map(this::replaceIfTs)
+                .collect(Collectors.joining("\n"));
+    }
+
+    private String replaceIfTs(String line) {
+        if (line.trim().endsWith(".ts")) {
+            return cloudFrontSigner.generateSignedUrlForVideo(line.trim());
+        }
+        return line;
     }
 }
