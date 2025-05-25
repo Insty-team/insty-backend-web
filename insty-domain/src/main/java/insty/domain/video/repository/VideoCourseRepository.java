a8d9dd6da53d2d0e98617427db32b22ce21c0e69
ajroot5685 <anjeong8577@naver.com>
2025-05-25 16:31:12 +0900

    🐛 Fix(영상 조회): 조회 오류 수정
    - uuid unique 설정
    - 단일 필드 조회 시 jpql 사용

--- insty-domain/src/main/java/insty/domain/video/repository/VideoCourseRepository.java
+++ insty-domain/src/main/java/insty/domain/video/repository/VideoCourseRepository.java
@@ -4,10 +4,13 @@ import insty.model.video.VideoCourse;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VideoCourseRepository extends JpaRepository<VideoCourse, Long> {
 
     Optional<VideoCourse> findByVideoUuid(UUID videoUuid);
 
-    Optional<UUID> findVideoUuidByCourseId(Long courseId);
+    @Query("SELECT vc.videoUuid FROM VideoCourse vc WHERE vc.courseId = :courseId AND vc.isDeleted = false")
+    Optional<UUID> findVideoUuidByCourseId(@Param("courseId") Long courseId);
 }
