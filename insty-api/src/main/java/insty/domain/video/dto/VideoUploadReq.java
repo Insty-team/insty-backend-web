31065cbdb76696b1efd0fe3980e456a2d5b03e20
ajroot5685 <anjeong8577@naver.com>
2025-05-27 15:46:39 +0900

    📝 Docs: 스웨거 필드 예시 추가

--- insty-api/src/main/java/insty/domain/video/dto/VideoUploadReq.java
+++ insty-api/src/main/java/insty/domain/video/dto/VideoUploadReq.java
@@ -1,15 +1,18 @@
 package insty.domain.video.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record VideoUploadReq(
+        @Schema(description = "파일명(mp4,mov,webm)", example = "fileName.mp4")
         @NotBlank
         @Pattern(
                 regexp = "^.+\\.[a-zA-Z0-9]+$",
                 message = "확장자 명이 포함된 파일 이름이여야 합니다."
         )
         String fileName,
+        @Schema(description = "영상 타입(video/mp4,video/quicktime,video/webm)", example = "video/mp4")
         @NotBlank
         @Pattern(
                 regexp = "^video/[a-zA-Z0-9!#$&.+\\\\-^_]+$",
