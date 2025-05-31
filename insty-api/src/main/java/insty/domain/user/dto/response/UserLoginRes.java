519c4865e331780ed8efc38d38575f1d87b21ed7
Ssspil <rius0918@gmail.com>
2025-06-01 01:00:25 +0900

    ✨ Feat(login): 로그인 응답 클래스명 변경

--- insty-api/src/main/java/insty/domain/user/dto/response/UserLoginRes.java
+++ insty-api/src/main/java/insty/domain/user/dto/response/UserLoginRes.java
@@ -1,7 +1,25 @@
 package insty.domain.user.dto.response;
 
+import insty.model.user.UserType;
+
 public record UserLoginRes(
-        int status,
-        String message
+        Long id,
+        String nickname,
+        UserType userType,
+        String accessToken,
+        String refreshToken,
+        long accessTokenExpiresIn,
+        long refreshTokenExpiresIn
 ) {
+    public static UserLoginRes create(
+            Long id,
+            String nickname,
+            UserType userType,
+            String accessToken,
+            String refreshToken,
+            long accessTokenExpiresIn,
+            long refreshTokenExpiresIn
+    ) {
+        return new UserLoginRes(id, nickname, userType, accessToken, refreshToken, accessTokenExpiresIn, refreshTokenExpiresIn);
+    }
 }
