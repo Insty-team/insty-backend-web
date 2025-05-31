50153b40eb51b7c8d0b6ffd5417426417bbbff5c
ajroot5685 <anjeong8577@naver.com>
2025-05-31 21:38:46 +0900

    ✅ Test(강의 상세조회): 단위테스트

--- insty-api/src/main/java/insty/domain/course/implement/CourseCounter.java
+++ insty-api/src/main/java/insty/domain/course/implement/CourseCounter.java
@@ -15,6 +15,12 @@ public class CourseCounter {
 
     private final CourseRepository courseRepository;
 
+    /**
+     * 동시성을 제어하기 위해 비관적 락, 수정 쿼리, 낙관적 락, Redis 등 여러가지 방식이 있다.<br> 가장 간단하고 실행 시간도 합리적인 수정 쿼리 방식을 사용한다.
+     *
+     * @param courseId
+     * @return
+     */
     public Course increaseViewCountAndGetCourse(Long courseId) {
         courseRepository.incrementViewCount(courseId);
         return courseRepository.findById(courseId)
