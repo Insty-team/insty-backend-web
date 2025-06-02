1c0d0344ea595c5f51479aa73cdb4f76595c721a
ajroot5685 <anjeong8577@naver.com>
2025-06-02 20:02:59 +0900

    ♻️ Refactor(파일): 파일 생성 요청 검증 추가

--- insty-api/src/main/java/insty/domain/common/FileCreateReq.java
+++ insty-api/src/main/java/insty/domain/common/FileCreateReq.java
@@ -1,5 +1,7 @@
 package insty.domain.common;
 
+import insty.error.CommonErrorCode;
+import insty.exception.CustomException;
 import insty.model.file.FileContainerType;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,4 +10,10 @@ public record FileCreateReq(
         FileContainerType containerType,
         Long containerId
 ) {
+
+    public FileCreateReq {
+        if (file.isEmpty() || containerType == null || containerId == null) {
+            throw new CustomException(CommonErrorCode.INVALID_FILE_CREATE_REQUEST);
+        }
+    }
 }
