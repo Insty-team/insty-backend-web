92fbdc435b0e3835f82acff2d7de516b7d4b9355
ajroot5685 <anjeong8577@naver.com>
2025-06-02 21:42:35 +0900

    ✅ Test(파일): 단위테스트

---
+++ insty-domain/src/test/java/insty/model/file/FileTest.java
@@ -0,0 +1,31 @@
+package insty.model.file;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("unit")
+class FileTest {
+
+    @Test
+    void create_정상() {
+        // given
+        FileContainerType containerType = FileContainerType.COURSE_THUMBNAIL;
+        Long containerId = 1L;
+        String name = "00000000-0000-0000-0000-000000000001.jpg";
+        String originalName = "fileName.jpg";
+        String contentType = "image/jpeg";
+
+        // when
+        File file = File.create(containerType, containerId, name, originalName, contentType);
+
+        // then
+        assertThat(file).isNotNull();
+        assertThat(file.getContainerType()).isEqualTo(containerType);
+        assertThat(file.getContainerId()).isEqualTo(containerId);
+        assertThat(file.getName()).isEqualTo(name);
+        assertThat(file.getOriginalName()).isEqualTo(originalName);
+        assertThat(file.getContentType()).isEqualTo(contentType);
+    }
+}
