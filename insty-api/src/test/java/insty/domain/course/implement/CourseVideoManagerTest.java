83bf3ced34c3e824f9b2a8e9aad925642502a780
ajroot5685 <anjeong8577@naver.com>
2025-06-04 21:14:57 +0900

    ✅ Test(강의 생성): 단위테스트

---
+++ insty-api/src/test/java/insty/domain/course/implement/CourseVideoManagerTest.java
@@ -0,0 +1,60 @@
+package insty.domain.course.implement;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import insty.domain.video.repository.VideoCourseRepository;
+import insty.model.course.Course;
+import insty.model.video.VideoCourse;
+import java.util.Optional;
+import java.util.UUID;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@Tag("unit")
+@ExtendWith(MockitoExtension.class)
+class CourseVideoManagerTest {
+
+    @InjectMocks
+    private CourseVideoManager courseVideoManager;
+
+    @Mock
+    private VideoCourseRepository videoCourseRepository;
+
+    @Test
+    void attachmentCourse_정상() {
+        // given
+        Course course = Course.create("제목", "설명", 10000, "강의 추천 대상자", true);
+        UUID videoUuid = UUID.fromString("00000000-0000-0000-0000-000000000001");
+
+        // mock
+        when(videoCourseRepository.findByVideoUuid(videoUuid))
+                .thenReturn(Optional.of(mock(VideoCourse.class)));
+
+        // when
+
+        // then
+        assertThatCode(() -> courseVideoManager.attachmentCourse(course, videoUuid))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void attachmentCourse_정상_영상을_업로드하지_않았다() {
+        // given
+        Course course = Course.create("제목", "설명", 10000, "강의 추천 대상자", true);
+        UUID videoUuid = null;
+
+        // mock
+
+        // when
+
+        // then
+        assertThatCode(() -> courseVideoManager.attachmentCourse(course, videoUuid))
+                .doesNotThrowAnyException();
+    }
+}
