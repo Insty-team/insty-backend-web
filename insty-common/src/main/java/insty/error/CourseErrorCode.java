fff8740a66c83b73414071419ec01c7d0aba219d
ajroot5685 <anjeong8577@naver.com>
2025-07-02 16:00:44 +0900

    ✨ Feat(강의 썸네일): 썸네일 타입 검증

--- insty-common/src/main/java/insty/error/CourseErrorCode.java
+++ insty-common/src/main/java/insty/error/CourseErrorCode.java
@@ -5,6 +5,7 @@ public enum CourseErrorCode implements ErrorCode {
     COURSE_TOO_MANY_PRACTICE_FILE("COURSE_002", "허용된 실습파일 개수를 초과했습니다.", 413),
     COURSE_CANT_CHANGE("COURSE_003", "강의 생성자가 아닙니다.", 403),
     COURSE_CREATE_ERROR("COURSE_004", "생성 메서드 검증에 실패했습니다.", 500),
+    COURSE_THUMBNAIL_INVALID_EXTENSION("COURSE_005", "썸네일은 jpg, jpeg, png만 업로드 가능합니다.", 400),
 
     ;
 
