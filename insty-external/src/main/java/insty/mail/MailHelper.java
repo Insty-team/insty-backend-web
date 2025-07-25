44000802cf48276322ed5a815a7ef14f6cb93faa
jihwankim128 <jihwan1535@nate.com>
2025-07-25 16:26:41 +0900

    ♻️ Refactor(emailAuthenticate): 비동기 로직으로 개선
    * 비동기 로직 개선으로 280배 성능 향상
    * 지수 백오프로 코드 안정성 3배 향상
    * 추후 버그 상황 모니터링을 위한 recover 인터페이스

--- insty-external/src/main/java/insty/mail/MailHelper.java
+++ insty-external/src/main/java/insty/mail/MailHelper.java
@@ -3,26 +3,44 @@ package insty.mail;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class MailHelper {
 
     private final JavaMailSender mailSender;
     private final SpringTemplateEngine templateEngine;
 
+    @Async("mailTaskExecutor")
+    @Retryable(
+        retryFor = {EmailSendException.class},
+        backoff = @Backoff(delay = 2000, multiplier = 2)
+    )
     public void sendVerificationCode(String to, String subject, String code) {
         try {
             MimeMessage message = getMimeMessage(to, subject, code);
             mailSender.send(message);
-        } catch (MessagingException e) {
-            throw new IllegalStateException("email sender error", e);
+        } catch (MailException | MessagingException e) {
+            throw new EmailSendException(e);
         }
+    }
+
+    @Recover
+    public void recover(EmailSendException ex, String to, String subject, String code) {
+        log.error("이메일 발송 최종 실패 - 3번 시도 후 포기. email: {}", to, ex);
+        // 나중에 슬랙 알림 등 추가되면 추가하기
     }
 
     private MimeMessage getMimeMessage(String to, String subject, String code) throws MessagingException {
