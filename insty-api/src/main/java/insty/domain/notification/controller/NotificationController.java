ab898742ac3a2acceee85f269b1ee3a8c219729c
SH Woo <imdlsrks.mc@gmail.com>
2025-11-13 09:34:13 +0900

    feat: 기본 서비스 및 컨트롤러 선언

---
+++ insty-api/src/main/java/insty/domain/notification/controller/NotificationController.java
@@ -0,0 +1,15 @@
+package insty.domain.notification.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/notification")
+@RequiredArgsConstructor
+public class NotificationController {
+
+
+}
---
+++ insty-api/src/main/java/insty/domain/notification/service/NotificationService.java
@@ -0,0 +1,20 @@
+package insty.domain.notification.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class NotificationService {
+
+
+    // todo : 사용자 알림 조회
+
+
+    // todo : 사용자 알림 읽음 처리 (redirect)
+
+
+}
