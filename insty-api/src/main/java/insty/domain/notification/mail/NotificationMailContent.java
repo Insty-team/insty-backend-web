a2ee0b72d2298ed86b8fe33b52d0918fa7ee3268
SH Woo <imdlsrks.mc@gmail.com>
2025-11-16 22:19:56 +0900

    ♻️ Refactor: 위치 리팩토링

---
+++ insty-api/src/main/java/insty/domain/notification/mail/NotificationMailContent.java
@@ -0,0 +1,31 @@
+package insty.domain.notification.mail;
+
+import insty.mail.MailType;
+import insty.mail.MailContent;
+import java.util.Map;
+
+/**
+ * 알림용 메일 컨텐츠
+ * NotificationRequest의 context를 그대로 템플릿 변수로 전달
+ */
+public class NotificationMailContent extends MailContent {
+
+    private final Map<String, Object> variables;
+
+    public NotificationMailContent(String to, MailType mailType, Map<String, Object> variables) {
+        super(to, mailType);
+        this.variables = variables;
+    }
+
+    @Override
+    public Map<String, Object> variables() {
+        return variables;
+    }
+
+    /**
+     * 빌더 패턴으로 생성
+     */
+    public static NotificationMailContent of(String to, MailType mailType, Map<String, Object> context) {
+        return new NotificationMailContent(to, mailType, context);
+    }
+}
