8bfc3bd7b1cc00fdf3cfa865880a08e4ccd246b7
SH Woo <imdlsrks.mc@gmail.com>
2025-11-16 21:50:00 +0900

    🐛 Fix: user 테스트 수정

--- insty-api/src/test/java/insty/domain/user/implement/UserWriterTest.java
+++ insty-api/src/test/java/insty/domain/user/implement/UserWriterTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
 
 @Tag("unit")
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +27,9 @@ class UserWriterTest {
 
     @Mock
     private CourseCleaner courseCleaner;
+
+    @Mock
+    private ApplicationEventPublisher eventPublisher;
 
     @InjectMocks
     private UserWriter userWriter;
