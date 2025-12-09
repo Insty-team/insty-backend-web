8df4e7e614eb2ed223b13a6e96eb27af2ecc50cd
lefoa <cynicalsky25@gmail.com>
2025-12-09 15:35:04 +0900

    🐛 Fix(auth): req code 관련 잘못된 pattern 삭제

--- insty-api/src/main/java/insty/domain/auth/dto/request/PasswordResetVerifyReq.java
+++ insty-api/src/main/java/insty/domain/auth/dto/request/PasswordResetVerifyReq.java
@@ -10,8 +10,6 @@ public record PasswordResetVerifyReq(
         String email,
 
         @NotBlank(message = "인증 코드를 입력해주세요.")
-        @Pattern(regexp="^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$",
-                 message="이메일 주소 양식을 확인해주세요")
         String code
 ) {
 }
