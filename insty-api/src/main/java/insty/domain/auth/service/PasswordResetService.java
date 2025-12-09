80e0b05f228117dc9fdada45a466e85b0ba710c3
lefoa <cynicalsky25@gmail.com>
2025-12-09 16:57:00 +0900

    ✨ Feat(auth): 도메인 서비스 및 통합 테스트 구현

---
+++ insty-api/src/main/java/insty/domain/auth/service/PasswordResetService.java
@@ -0,0 +1,74 @@
+package insty.domain.auth.service;
+
+import insty.domain.auth.dto.response.PasswordResetMailRes;
+import insty.domain.auth.dto.response.PasswordResetVerifyRes;
+import insty.domain.auth.dto.response.PasswordUpdateRes;
+import insty.domain.auth.implement.emailverification.MailAuthenticateContent;
+import insty.domain.auth.implement.passwordreset.PasswordResetReader;
+import insty.domain.auth.implement.passwordreset.PasswordResetWriter;
+import insty.domain.user.implement.UserReader;
+import insty.domain.user.implement.UserValidator;
+import insty.error.AuthErrorCode;
+import insty.error.UserErrorCode;
+import insty.exception.CustomException;
+import insty.mail.MailHelper;
+import insty.model.auth.PasswordResetVerification;
+import insty.model.auth.TokenGenerator;
+import insty.model.user.User;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import java.time.LocalDateTime;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Validated
+public class PasswordResetService {
+
+    private final PasswordResetWriter passwordResetWriter;
+    private final PasswordResetReader passwordResetReader;
+    private final MailHelper mailHelper;
+    private final UserValidator userValidator;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final UserReader userReader;
+    private final TokenGenerator tokenGenerator;
+
+    public PasswordResetMailRes sendResetMail(@NotNull String email){
+        if(userValidator.existsEmail(email)){
+            PasswordResetVerification passwordResetVerification = passwordResetReader.findOptionalByEmail(email)
+                    .map(verification -> verification.reissue(tokenGenerator))
+                    .orElse(PasswordResetVerification.create(email, tokenGenerator));
+
+            MailAuthenticateContent mailContent = MailAuthenticateContent.of(email, passwordResetVerification.getCode());
+            mailHelper.send(mailContent);
+            passwordResetWriter.save(passwordResetVerification);
+            return PasswordResetMailRes.from(email,passwordResetVerification.getExpiredAt());
+        }else{
+            throw new CustomException(UserErrorCode.USER_NOT_FOUND);
+        }
+    }
+
+    public PasswordResetVerifyRes verifyCode(@NotNull String email, @NotNull String code){
+        PasswordResetVerification verification = passwordResetReader.findByEmail(email);
+        verification.verify(code);
+        passwordResetWriter.save(verification);
+        return PasswordResetVerifyRes.from(email,verification.isVerified(),verification.getExpiredAt());
+    }
+
+    public PasswordUpdateRes updatePassword(@NotNull String email, @NotNull String newPassword) {
+
+        if (!passwordResetReader.isVerified(email)) {
+            throw new CustomException(AuthErrorCode.EMAIL_NOT_VERIFIED);
+        }
+        User userByEmail = userReader.getUserByEmail(email);
+        userByEmail.changePassword(passwordEncoder.encode(newPassword));
+        return PasswordUpdateRes.from(email,true, LocalDateTime.now());
+    }
+
+
+}
