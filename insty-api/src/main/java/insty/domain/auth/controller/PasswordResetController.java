bcb7daaa0bad557e2b76f65c1e0636130d6c843a
lefoa <cynicalsky25@gmail.com>
2025-12-10 16:51:33 +0900

    🐛 Fix(auth): 클래스명 오타 수정

--- insty-api/src/main/java/insty/domain/auth/controller/PasswordResetController.java
+++ insty-api/src/main/java/insty/domain/auth/controller/PasswordResetController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
-public class PasswordRestController {
+public class PasswordResetController {
 
     private final PasswordResetService passwordResetService;
 
