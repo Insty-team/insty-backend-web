6a99004d27b1655a972eefdb26ce89c7b77e72b8
SH Woo <imdlsrks.mc@gmail.com>
2025-12-27 15:26:32 +0900

    ♻️ Refactor: 커뮤니티 컨트롤러 게시글/댓글 분리

--- insty-api/src/main/java/insty/domain/community/controller/CommunityCommentController.java
+++ insty-api/src/main/java/insty/domain/community/controller/CommunityCommentController.java
@@ -5,13 +5,7 @@ import insty.domain.community.dto.CommunityCommentCreateReq;
 import insty.domain.community.dto.CommunityCommentRes;
 import insty.domain.community.dto.CommunityCommentSearchReq;
 import insty.domain.community.dto.CommunityCommentUpdateReq;
-import insty.domain.community.dto.CommunityPostCreateReq;
-import insty.domain.community.dto.CommunityPostDetailsRes;
-import insty.domain.community.dto.CommunityPostRes;
-import insty.domain.community.dto.CommunityPostSearchReq;
-import insty.domain.community.dto.CommunityPostUpdateReq;
 import insty.domain.community.service.CommunityCommentService;
-import insty.domain.community.service.CommunityPostService;
 import insty.global.annotation.CurrentUser;
 import insty.global.response.SuccessRes;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -34,66 +28,14 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Tag(name = "커뮤니티 API", description = "커뮤니티 게시글/댓글 작성 및 조회 API")
+@Tag(name = "커뮤니티 댓글 API", description = "커뮤니티 댓글 작성 및 조회 API")
 @Validated
 @RestController
 @RequestMapping("/api/v1/community")
 @RequiredArgsConstructor
-public class CommunityController {
+public class CommunityCommentController {
 
-    private final CommunityPostService communityPostService;
     private final CommunityCommentService communityCommentService;
-
-    // ----------------------- 게시글 -----------------------
-
-    @PreAuthorize("hasRole('LEARNER') or hasRole('CREATOR')")
-    @GetMapping("/posts")
-    public SuccessRes<SearchRes<CommunityPostRes>> searchPosts(
-            @ModelAttribute @Valid CommunityPostSearchReq req
-    ) {
-        return SuccessRes.of(communityPostService.searchPosts(req));
-    }
-
-    @PreAuthorize("hasRole('LEARNER') or hasRole('CREATOR')")
-    @GetMapping("/posts/{postId}")
-    public SuccessRes<CommunityPostDetailsRes> getPost(
-            @PathVariable @NotNull Long postId
-    ) {
-        return SuccessRes.of(communityPostService.getPostDetails(postId));
-    }
-
-    @PreAuthorize("hasRole('LEARNER') or hasRole('CREATOR')")
-    @PostMapping(value = "/posts", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public SuccessRes<CommunityPostDetailsRes> createPost(
-            @CurrentUser Long userId,
-            @RequestPart("post") @Valid CommunityPostCreateReq req,
-            @RequestPart(value = "attachments", required = false) @Size(max = 2) List<MultipartFile> attachments
-    ) {
-        return SuccessRes.of(communityPostService.createPost(userId, req, attachments));
-    }
-
-    @PreAuthorize("hasRole('LEARNER') or hasRole('CREATOR')")
-    @PatchMapping(value = "/posts/{postId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public SuccessRes<CommunityPostDetailsRes> updatePost(
-            @CurrentUser Long userId,
-            @PathVariable @NotNull Long postId,
-            @RequestPart("post") @Valid CommunityPostUpdateReq req,
-            @RequestPart(value = "attachments", required = false) @Size(max = 2) List<MultipartFile> attachments
-    ) {
-        return SuccessRes.of(communityPostService.updatePost(userId, postId, req, attachments));
-    }
-
-    @PreAuthorize("hasRole('LEARNER') or hasRole('CREATOR')")
-    @DeleteMapping("/posts/{postId}")
-    public SuccessRes<?> deletePost(
-            @CurrentUser Long userId,
-            @PathVariable @NotNull Long postId
-    ) {
-        communityPostService.deletePost(userId, postId);
-        return SuccessRes.of(null);
-    }
-
-    // ----------------------- 댓글 -----------------------
 
     @PreAuthorize("hasRole('LEARNER') or hasRole('CREATOR')")
     @GetMapping("/posts/{postId}/comments")
